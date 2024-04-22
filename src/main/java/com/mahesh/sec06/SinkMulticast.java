33542de698a8dd96cbd0e173d7ac5e7f8a76afea
mahesh <mahesh.kamineni35@gmail.com>
2024-04-21 19:21:35 -0500

    commit

--- src/main/java/com/mahesh/sec06/SinkMulticast.java
+++ src/main/java/com/mahesh/sec06/SinkMulticast.java
@@ -1,4 +1,18 @@
 package com.mahesh.sec06;
 
+import courseutil.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
 public class SinkMulticast {
+    public static void main(String[] args) {
+        Sinks.Many<Object> sinks=Sinks.many().multicast().onBackpressureBuffer();
+        Flux<Object> flux=sinks.asFlux();
+        flux.subscribe(Util.onNext());
+        flux.subscribe(Util.onNext());
+        flux.subscribe(Util.onNext());
+        sinks.tryEmitNext("hi");
+        sinks.tryEmitNext("how are you");
+    }
+
 }
